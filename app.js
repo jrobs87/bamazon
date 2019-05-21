c1616550fdec0f1e0ca28a5950bae3fdb19acb91
John Robertson <tiktaalikdesign@gmail.com>
2019-05-21 10:52:15 -0400

    v1.1

--- app.js
+++ app.js
@@ -1,84 +1,144 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
+// array to hold our items
+let items = [];
+let order = '';
+let quantity = '';
+
+// item object constructer
+function Item(id, name, price, dept, stock) {
+  this.id = id;
+  this.name = name;
+  this.price = price;
+  this.department = dept;
+  this.stock = stock;
+};
+
 console.log("");
 console.log("Welcome to Bamazon!")
-console.log("----- ----- -----");
+console.log("----------------------------------------------------------------------");
 
+// db connection setup
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "docker",
-    database: "bamazon"
-    
-  });
+  host: "localhost",
 
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
+  // Your port; if not 3306
+  port: 3306,
 
-    queryAllProducts();
-    
-  console.log("----- ----- -----");
-  });
+  // Your username
+  user: "root",
 
-function queryAllProducts() {
+  // Your password
+  password: "docker",
+  database: "bamazon"
+
+});
+
+// connecting to the db and querying the products
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  console.log("----------------------------------------------------------------------");
+  queryAllProducts(); // retrieve all products from db
+});
+
+// defining the query all function
+function queryAllProducts(res, err) {
   console.log("AVAILABLE PRODUCTS");
-  connection.query("SELECT * FROM products", function(err, res) {
+  console.log("----------------------------------------------------------------------");
+  
+  // log out all products to console
+  connection.query("SELECT * FROM products", function (err, res) {
+
+    // display the product choices
     for (var i = 0; i < res.length; i++) {
       console.log(`${res[i].item_id} - ${res[i].product_name} - ${res[i].price} - ${res[i].department_name} - ${res[i].stock_quantity}`);
+
+      // creating new item objects
+      let item = new Item(res[i].item_id, res[i].product_name, res[i].price, res[i].department_name, res[i].stock_quantity);
+      items.push(item); // adding to items array for inquirer
+      // console.log(items);
     };
-    console.log("-----------------------------------");
+
+    console.log("----------------------------------------------------------------------");    
+    productSelection();
   });
-  connection.end();
+
+  connection.end(); // terminate connection
 }
 
-  // rename to bamazonCustomer.js
+// function to grab user input for product selection
+productSelection = function() {
+  inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'item',
+      message: 'What item would you like to order?',
+      choices: function() {
+        choices = [];
+        for (i = 0; i < items.length; i++) {
+          choices.push(items[i].name);
+        }
+        return choices;
+      }
+    },
+    {
+      type: 'input',
+      name: 'quantity',
+      message: 'How many would you like to order?',
+      validate: function validateQuanity(name){
+        // prevents null or zero entries
+        return name > 0;
+    }
+      }
+    
+  ])
+  .then(answers => {
+    console.log("----------------------------------------------------------------------");
+    // console.log(`Order: ${answers.quantity} of ${answers.item}`);
+    const index = items.map(e => e.name).indexOf(answers.item);
+    order = items[index];
+    quantity = answers.quantity;
 
-  // Create a "Prompt" with a series of questions.
-// inquirer
-// .prompt([
-//   // Here we create a basic text prompt.
-//   {
-//     type: "input",
-//     message: "What is your name?",
-//     name: "username"
-//   },
-//   // Here we create a basic password-protected text prompt.
-//   {
-//     type: "password",
-//     message: "Set your password",
-//     name: "password"
-//   },
-//   // Here we give the user a list to choose from.
-//   {
-//     type: "list",
-//     message: "Which Pokemon do you choose?",
-//     choices: ["Bulbasaur", "Squirtle", "Charmander"],
-//     name: "pokemon"
-//   },
-//   // Here we ask the user to confirm.
-//   {
-//     type: "confirm",
-//     message: "Are you sure:",
-//     name: "confirm",
-//     default: true
-//   }
-// ])
-// .then(function(inquirerResponse) {
-//   // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
-//   if (inquirerResponse.confirm) {
-//     console.log("\nWelcome " + inquirerResponse.username);
-//     console.log("Your " + inquirerResponse.pokemon + " is ready for battle!\n");
-//   }
-//   else {
-//     console.log("\nThat's okay " + inquirerResponse.username + ", come again when you are more sure.\n");
-//   }
-// });
+    // check for quantity
+if (items[index].stock < answers.quantity) {
+  console.log(`Oh no! There are only ${items[index].stock} of ${answers.item} in stock.`);
+  console.log('Please revise your order from the products below.')
+  console.log("----------------------------------------------------------------------");
+
+  // redisplay items
+  for (i = 0; i < items.length; i++)  {
+  console.log(`Item: ${items[i].name} | Price: $ ${items[i].price} | Department: ${items[i].department} | ${items[i].stock} in stock.`);
+  }
+  console.log("----------------------------------------------------------------------");
+  // revise  order if quantity exceeds stock
+  productSelection();
+} else {
+  console.log(`Your order of ${quantity} ${order.name} for ${((order.price * quantity) * 1.06).toFixed(2)} is ready to be placed.`)
+  console.log("----------------------------------------------------------------------");
+  inquirer
+  .prompt([
+    {
+      type: 'confirm',
+      name: 'placeOrder',
+      message: 'Place this order?',
+      }
+  ])
+  .then(answers => {
+    if (answers.placeOrder === true) {
+      console.log("----------------------------------------------------------------------");
+      console.log(`Your order for ${quantity} ${order.name} has been placed.`);
+      console.log("----------------------------------------------------------------------");
+    }
+  });
+}
+  });
+}
+
+// productUpdate = function() {
+//   connection.query
+// }
+// rename to bamazonCustomer.js
+
